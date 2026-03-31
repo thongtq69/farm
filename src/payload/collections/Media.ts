aeb59db97c8c35cbfbdda461a0f60e77be9624a0
thongtq69 <thongtqse173366@fpt.edu.vn>
2026-03-31 18:14:22 +0700

    S

--- src/payload/collections/Media.ts
+++ src/payload/collections/Media.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
+  admin: {
+    hidden: true,
+  },
   upload: {
     staticDir: 'public/media',
     adminThumbnail: 'thumbnail',
